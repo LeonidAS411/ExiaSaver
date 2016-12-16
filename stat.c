b7b999050d181427e717094b3b62897c4ff277a3
LeonidAS411 <leon.schaal@viacesi.fr>
2016-12-16 15:57:49 +0100

    Add files via upload

--- stat.c
+++ stat.c
@@ -4,115 +4,137 @@
 #include <string.h>
 #define TAILLE_HISTORIQUE 20
 
-int main(int argc, char * argv[])
+void historique() //creation de la fonction historique
 {
-    FILE *historique = NULL;
+    FILE *historique = NULL; //on met on pointeur sur le fichier historique
+    historique = fopen("historique.txt", "r"); //ouverture du fichier historique
 
-    char caractere = 0;
+    int tab[10][7]; // Variable contenant la chaine lu
 
-    historique = fopen("htc.txt", "r");
+    int j,k,l; //Variable pour l'algo de tri
+    int tri=0;
 
-    int tab[7][10];
+    char nom[50]; // Variable contenant le nom de l'image
 
-    char nom[50];
+    int compteurMode[3] = {0}; // Variable contenant le compteur de chaque mode
+    int compteurImage[5] = {0}; // Variable contenant le compteur de chaque image
+    int choix = 0, i = 0; //Variable contenant le choix de la statistique lu par l'utilisateur
 
-    int i;
-    int compteurMode[3] = {0};
-    int compteurImage[5] = {0};
-    int choix = 0;
-
-
-
-
-    /**
-        OUVERTURE ET LECTURE DU FICHIER
-    */
 
     /// SI ON ARRIVE A OUVRIR LE FICHIER
     if (historique != NULL)
     {
-        for (i = 0; i < 10; i++)
+
+
+	system("clear"); //la console se vide
+
+	printf("Bienvenue dans le menu statistique\n\nQuelle statistique voulez vous observer ? \n\n 1. Les types lances par ExiaSaver\n 2. Les images.pbm lancees par le type statique\n"); //Menu des statistiques
+        scanf("%d", &choix); //L'utilisateur rentre un chiffre qui correspond à une statistique
+
+        switch(choix)
         {
-            fscanf(historique, "%d/%d/%d %d:%d:%d;%d; %s", &tab[0][i], &tab[1][i], &tab[2][i], &tab[3][i], &tab[4][i], &tab[5][i], &tab[6][i], &nom);
-            //printf("%d\n", tab[6][i]);
-            printf("%s\n", nom);
+            case 1: //si l'utilisateur a rentré "1"
+                for (i = 0; i < 10; i++) //parcours du fichier historique
+                {
+                    fscanf(historique, "%d/%d/%d %d:%d:%d;%d; %s", &tab[i][0], &tab[i][1], &tab[i][2], &tab[i][3], &tab[i][4], &tab[i][5], &tab[i][6], &nom); //scan des valeurs du fichier historique
 
+                    if (tab[i][6] == 1) //Type statique
+                        compteurMode[0]++;
+                    else if (tab[i][6] == 2) //Type dynamique
+                        compteurMode[1]++;
+                    else if (tab[i][6] == 3) //Type intéractif
+                        compteurMode[2]++;
+                }
+                printf("Type statique : %d\nType dynamique : %d\nType interactif : %d\n", compteurMode[0], compteurMode[1], compteurMode[2]); //Affichage du nombre de lancement de chaque types
+            break;
 
-            printf("Bienvenue dans le menu statistique\n\nQuelle statistique voulez vous observer ? \n\n 1. Les types lances par ExiaSaver\n 2. Les images.pbm lancees par le type statique\n");
-            scanf("%d", &choix);
+            case 2: //si l'utilisateuur tape "2"
+                for (i = 0; i < 10; i++)//parcours de l'historique
+                {
+                    fscanf(historique, "%d/%d/%d %d:%d:%d;%d; %s", &tab[i][0], &tab[i][1], &tab[i][2], &tab[i][3], &tab[i][4], &tab[i][5], &tab[i][6], &nom); //scan des valeurs du fichier historique
 
-            switch (choix)
-            {
-                /**
-                    STATISTIQUES 1
-                    INDIQUE LE NOMBRE D'AFFICHAGE POUR CHAQUE MODE
-                */
-                case 1:
-                    for(i=0;i<5; i++)
-                    {
-                        if (tab[6][i] == 1)
-                            compteurMode[0]++;
-                        else if (tab[6][i] == 2)
-                            compteurMode[1]++;
-                        else if (tab[6][i] == 3)
-                            compteurMode[2]++;
-                    }
-                    printf("Type statique : %d\nType dynamique : %d\nType intéractif : %d\n", compteurMode[0], compteurMode[1], compteurMode[2]);
-                break;
-
-
-
-                /**
-                    STATISTIQUES 2
-                    INDIQUE LE NOMBRE D'AFFICHAGE POUR CHAQUE IMAGE
-                */
-
-                case 2:
-                    if (strcmp(nom, "fusee.pbm") == 0)
+                    if (strcmp(nom, "fusee.pbm") == 0) //compare pour trouver le nom du fichier
                     {
                         compteurImage[0]++;
                     }
 
-                    else if (strcmp(nom, "panda.pbm") == 0)
+                    else if (strcmp(nom, "panda.pbm") == 0) //......
                     {
                         compteurImage[1]++;
                     }
 
-                    else if (strcmp(nom, "temps.pbm") == 0)
+                    else if (strcmp(nom, "temps.pbm") == 0)//......
                     {
                         compteurImage[2]++;
                     }
 
-                    else if (strcmp(nom, "main.pbm") == 0)
+                    else if (strcmp(nom, "main.pbm") == 0)//......
                     {
                         compteurImage[3]++;
                     }
 
-                    else if (strcmp(nom, "cadenas.pbm") == 0)
+                    else if (strcmp(nom, "cadenas.pbm") == 0) //......
                     {
                         compteurImage[4]++;
                     }
+                }
+                printf("fusee : %d\npanda : %d\ntemps : %d\nmain : %d\ncadenas : %d\n", compteurImage[0], compteurImage[1], compteurImage[2], compteurImage[3], compteurImage[4]); //affichage final du menu 2
+            break;
 
-                    printf("Image de la fusee : %d\nImage du panda : %d\nImage du sablier : %d\nImage de la main : %d\nImage du cadenas : %d\n", compteurImage[0], compteurImage[1], compteurImage[2], compteurImage[3], compteurImage[4]);
+        }
 
-                break;
+    /*case3:
 
+       // while (tri==0)
+        {
+            int i = 0;
+            tri = 1;
+  	    if(tab[i][6] l< tab[i+1][6])
+            {
+                int tampon=0;
+
+                tampon = tab[i][6];
+
+                tab[i][6]] = tab[i+1][6];
+
+                tab[i+1][6] = tampon;
+
+                tri = 0;
             }
         }
+        for (k=0;k<10;k++){
+		for (l=0;l<7;l++){
+			printf("%d",tab[k][l]);
+		}
+		printf("/n");
+	} */
+
+
+    fclose(historique);
+
     }
 
-    /// SINON
+ /// SINON
     else
     {
         printf("Impossible d'ouvrir le fichier");
     }
 
+}
 
 
+int main(int argc, char *argv[])
+{
 
-//system("clear");
-
-
-    fclose(historique);
+    if (strcmp(argv[1], "-stats") == 0)
+    {
+        historique();
+    }
+    else
+    {
+	printf("Utilisez -stats pour lancer le mode statistique\n");
+    }
     return 0;
 }
+
+
